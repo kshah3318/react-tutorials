f4dc0ba96370ec7c426844192cd6c866528b75e3
kshah3318 <kshah3318@gmail.com>
2018-05-27 18:10:41 +0530

    Clock App

--- src/index.js
+++ src/index.js
@@ -8,13 +8,20 @@ import ReactDOM from 'react-dom'
 }
 let name = 'Hem Shah'
 const element = <h1>Welcome to React {name}</h1>*/
-function  cartoon(name='Pokemon',show='11:00 am'){
+/*function  cartoon(name='Pokemon',show='11:00 am'){
   return <div>
   <h1 className="some-class">Cartoon name is {name} and its show is {show}</h1>
   <h2>Hello Karan</h2>
   </div>
+}*/
+
+//variable initiated with function
+let clock = function(){
+  return <h2>Time now : {new Date().toLocaleTimeString()}</h2>
 }
-ReactDOM.render(
-  cartoon('Tom and Jerry','10:am'),
-  document.getElementById('root')
-);
+setInterval(function(){
+  ReactDOM.render(
+    clock(),
+    document.getElementById('root')
+  );
+},1000)
