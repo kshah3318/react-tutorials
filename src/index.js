ad0b5c0685352a630a2062ec0cbc8a0a989cb547
kshah3318 <kshah3318@gmail.com>
2018-05-27 17:31:14 +0530

    Second Commite

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+/*let obj={
+  name:'Karan',
+  lname:'Shah'
+}
+let name = 'Hem Shah'
+const element = <h1>Welcome to React {name}</h1>*/
+function  cartoon(name='Pokemon',show='11:00 am'){
+  return <div>
+  <h1 className="some-class">Cartoon name is {name} and its show is {show}</h1>
+  <h2>Hello Karan</h2>
+  </div>
+}
 ReactDOM.render(
-  <h1>Welcome Karan Shah</h1>,
+  cartoon('Tom and Jerry','10:am'),
   document.getElementById('root')
 );
